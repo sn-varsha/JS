8bed87cc9f0dcae2a719b7ebef665a413c010735
sn-varsha <varshasn1231@gmail.com>
2025-08-15 17:36:54 +0530

    learnt comparison operator with equality and strick equality operator

---
+++ comparisonOperator/comparison.js
@@ -0,0 +1,63 @@
+//Boolean & Number
+console.log(true==1);
+console.log(true===1);
+
+console.log(false==0);
+console.log(false===0);
+
+//Boolean & String
+console.log(true=='1');
+console.log(true==='1');
+
+console.log(false=='0');
+console.log(false==='0');
+
+//Null & Undefined
+console.log(null==undefined);
+console.log(null===undefined);
+
+//Empty String & Zero
+console.log(""==0);
+console.log(""===0);
+
+console.log(" "==0);
+console.log(" "===0);
+
+//Boolean & Empty String
+console.log(false=='');
+console.log(false==='');
+
+console.log(true=='');
+console.log(true==='');
+
+//Null & Boolean
+console.log(null==true);
+console.log(null===true);
+
+console.log(null==false);
+console.log(null===false);
+
+//Undefined & Zero
+console.log(undefined==0);
+console.log(undefined===0);
+
+//NaN
+console.log(NaN==NaN);
+console.log(NaN==NaN);
+
+//String & Boolean
+console.log('true'==true);
+console.log('false'==false);
+
+//String no. & Number
+console.log('001'==1);
+console.log('001'===1);
+
+console.log('010'==10);
+console.log('010'===10);
+
+//Number & Null
+console.log(0==null);
+
+//Number & Undefined
+console.log(0==undefined);
---
+++ comparisonOperator/equalityAndStrictEquality.js
@@ -0,0 +1,15 @@
+let userName='ann';
+let enteredUserName = 'Ann';
+console.log(userName == enteredUserName);
+
+let userName1='ann';
+let enteredUserName1 = 'ann';
+console.log(userName1 == enteredUserName1);
+
+let userId ='5';
+let enteredUserId = 5;
+console.log(userId == enteredUserId);
+
+let userId1 ='5';
+let enteredUserId1 = 5;
+console.log(userId1 === enteredUserId1);
