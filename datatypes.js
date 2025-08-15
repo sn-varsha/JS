d63465351dc6df33688af6d4a05270fae4753210
sn-varsha <varshasn1231@gmail.com>
2025-08-15 15:40:59 +0530

    Different datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,51 @@
+let a=23;
+console.log(a);
+console.log(typeof a);
+
+let b=1/0;
+console.log(b);
+console.log(typeof b);
+
+let c=-1/0;
+console.log(c);
+console.log(typeof c);
+
+let d=6.8;
+console.log(d);
+console.log(typeof d);
+
+let e="hello"/2;
+console.log(e);
+console.log(typeof e);
+
+let f=100n;
+console.log(f);
+console.log(typeof f);
+
+let g="mee";
+console.log(g);
+console.log(typeof g);
+
+let h=true;
+console.log(h);
+console.log(typeof h);
+
+let i=false;
+console.log(i);
+console.log(typeof i);
+
+let j;
+console.log(j);
+console.log(typeof j);
+
+let k=0/0;
+console.log(k);
+console.log(typeof k);
+
+let l=0;
+console.log(l);
+console.log(typeof l);
+
+let m=1;
+console.log(m);
+console.log(typeof m);
