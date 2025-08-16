76ef18cfdde39bbffe653b026ef0de1055cc0d1a
sn-varsha <varshasn1231@gmail.com>
2025-08-16 10:18:45 +0530

    learnt about hoisting

---
+++ hoisting/hoistingFunc.js
@@ -0,0 +1,10 @@
+greet();
+function greet(){
+    console.log("hellooo");
+}
+
+// it will be treated as
+// function greet(){
+//     console.log("hellooo");
+// }
+// greet();
---
+++ hoisting/letVsVar.js
@@ -0,0 +1,8 @@
+console.log(b);
+var b=100;
+console.log(b);
+
+console.log(a);
+let a=100;
+console.log(a);
+
